26afe306204fb45c10c455a4486f593807233f1f
jadjer <jadjer@ya.ru>
2021-10-10 00:39:00 +0300

    Extern config file

--- ansem/__init__.py
+++ ansem/__init__.py
@@ -7,25 +7,11 @@ from .api import api_v1
 from .secury import identity, authentication
 from .models import db
 
-jwt = JWT(authentication_handler=authentication, identity_handler=identity)
 
-
-def create_app(test_config=None):
+def create_app():
     # create and configure the app
     app = Flask(__name__, instance_relative_config=True)
-    app.config.from_mapping(
-        SECRET_KEY=b'e02d3a100e849c3cf396',
-        # DATABASE=os.path.join(app.instance_path, 'database.sqlite'),
-        SQLALCHEMY_DATABASE_URI='sqlite:////home/jadjer/database.sqlite',
-        SQLALCHEMY_TRACK_MODIFICATIONS=False
-    )
-
-    if test_config is None:
-        # load the instance config, if it exists, when not testing
-        app.config.from_pyfile('config.py', silent=True)
-    else:
-        # load the test config if passed in
-        app.config.from_mapping(test_config)
+    app.config.from_pyfile(os.path.join(app.instance_path, 'application.cfg'))
 
     # ensure the instance folder exists
     try:
@@ -33,13 +19,13 @@ def create_app(test_config=None):
     except OSError:
         pass
 
+    JWT(app, authentication, identity)
+
     db.init_app(app)
-    jwt.init_app(app)
 
     with app.app_context():
         db.create_all()
 
-    # app.register_blueprint(auth)
     app.register_blueprint(api_v1)
 
     return app
