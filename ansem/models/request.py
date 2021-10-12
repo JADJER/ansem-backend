289e5f56336bb760e1c3020dae3cf1672b22d38b
jadjer <jadjer@ya.ru>
2021-10-12 22:57:03 +0300

    Edit models

--- ansem/models/request.py
+++ ansem/models/request.py
@@ -8,39 +8,28 @@ class Request(db.Model):
     __tablename__ = 'requests'
 
     id: int = db.Column(db.Integer, primary_key=True)
-    country: str = db.Column(db.String, nullable=False, default="Belarus")
-    city: str = db.Column(db.String, nullable=False)
-    address: str = db.Column(db.String, nullable=False)
     school: str = db.Column(db.String, nullable=False)
+    class_no: str = db.Column(db.String, nullable=False)
     score: float = db.Column(db.Float)
     index: int = db.Column(db.Integer)
 
     user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
     user = db.relationship('User', foreign_keys=user_id)
 
+    json = {
+        'id': id,
+        'user_id': user_id,
+        'school': school,
+        'class': class_no,
+        'score': score,
+        'index': index
+    }
+
     def __init__(self, **kwargs):
         super(Request, self).__init__(**kwargs)
 
     def __repr__(self):
-        return {
-            'id': self.id,
-            'country': self.email,
-            'city': self.first_name,
-            'address': self.last_name,
-            'school': self.last_name,
-            'score': self.last_name,
-            'index': self.last_name,
-            'user_id': self.last_name
-        }
+        return self.json
 
     def __str__(self):
-        return {
-            'id': self.id,
-            'country': self.email,
-            'city': self.first_name,
-            'address': self.last_name,
-            'school': self.last_name,
-            'score': self.last_name,
-            'index': self.last_name,
-            'user_id': self.last_name
-        }
+        return self.json
--- ansem/models/user.py
+++ ansem/models/user.py
@@ -8,26 +8,31 @@ class User(db.Model):
     __tablename__ = 'users'
 
     id: int = db.Column(db.Integer, primary_key=True)
-    email: str = db.Column(db.String(120), unique=True, nullable=False)
-    password: str = db.Column(db.String(150), nullable=False)
-    first_name: str = db.Column(db.String(150), nullable=True)
-    last_name: str = db.Column(db.String(150), nullable=True)
+    email: str = db.Column(db.String, unique=True, nullable=False)
+    password: str = db.Column(db.String, nullable=False)
+    first_name: str = db.Column(db.String, nullable=False)
+    last_name: str = db.Column(db.String, nullable=False)
+    country: str = db.Column(db.String, nullable=False, default="Belarus")
+    city: str = db.Column(db.String, nullable=False)
+    address: str = db.Column(db.String, nullable=False)
+    mobile_no: str = db.Column(db.String, unique=True, nullable=False)
+
+    json = {
+        'id': id,
+        'email': email,
+        'first_name': first_name,
+        'last_name': last_name,
+        'country': country,
+        'city': city,
+        'address': address,
+        'mobile_no': mobile_no,
+    }
 
     def __init__(self, **kwargs):
         super(User, self).__init__(**kwargs)
 
     def __repr__(self):
-        return {
-            'id': self.id,
-            'email': self.email,
-            'first_name': self.first_name,
-            'last_name': self.last_name
-        }
+        return self.json
 
     def __str__(self):
-        return {
-            'id': self.id,
-            'email': self.email,
-            'first_name': self.first_name,
-            'last_name': self.last_name
-        }
+        return self.json
